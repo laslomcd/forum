778b491fb9c8bffdca617d710f7043caa065d5bd
ryan <laslomcd@gmail.com>
2020-06-15 19:57:46 -0400

    installed font awesome

--- public/js/app.js
+++ public/js/app.js
@@ -49839,8 +49839,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Volumes/Elements/Code/PHP/forum/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Volumes/Elements/Code/PHP/forum/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Volumes/Transcend/Development/Laravel Projects/forum/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Volumes/Transcend/Development/Laravel Projects/forum/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,20 +5,57 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h2>{{ $question->title }}</h2>
-                            <div class="ml-auto">
-                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Questions</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h2>{{ $question->title }}</h2>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Questions</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="card-body">
-                        {!! $question->body_html !!}
+                        <hr>
+
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="This question is useful" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="votes-count">1234</span>
+                                <a title="This question is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Click to mark as favorite" class="favorite mt-2 favorited">
+                                    <i class="fas fa-star fa-2x"></i>
+                                    <span class="favorites-count">222</span>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!! $question->body_html !!}
+                                <div class="float-right mt-4">
+                                    <span class="text-muted">Asked: {{ $question->created_date }}</span>
+                                    <div class="media mt-3">
+                                        <a href="{{ $question->user->url }}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}" alt="">
+                                        </a>
+                                        <div class="media-body mt-2">
+                                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        @include('answers.index', [
+            'answers' => $question->answers,
+            'answersCount' => $question->answers_count
+        ])
+        @include('answers.create')
+
     </div>
 @endsection
